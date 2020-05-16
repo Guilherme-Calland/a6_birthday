1db2b608ed7ab6cf0d8ba5c551988d34852b45bb
guilhermow <gui.crb0@gmail.com>
2020-05-16 17:23:16 -0300

    commit inicial

---
+++ android/app/src/main/kotlin/com/guilhermecallandprojects/a6_birthday/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.guilhermecallandprojects.a6_birthday
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:a6_birthday/screens/menu.screen.dart';
+import 'package:a6_birthday/screens/start.screen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(BirthDay());
+}
+
+class BirthDay extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.red,
+        secondaryHeaderColor: Colors.red
+      ),
+      initialRoute: 'start.screen',
+      routes: {
+        'menu.screen' : (context) => MenuScreen(),
+        'start.screen' : (context) => StartScreen(),
+      }
+    );
+  }
+}
